012fe6e0b4bfca9d62a9c7dddd7eab6fc5d28e02
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-07-28 15:26:05 -0300

    Cruds com scaffolding

---
+++ WebSite/Controllers/AlunoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DAO.Dao;
+using Model.Models;
+
+namespace WebSite.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private EscolaWebContext db = new EscolaWebContext();
+
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            return View(db.Alunos.ToList());
+        }
+
+        // GET: Aluno/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aluno aluno = db.Alunos.Find(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        // GET: Aluno/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Aluno/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NrMatricula,Nome,CPF,DataNascimento,Status")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alunos.Add(aluno);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aluno);
+        }
+
+        // GET: Aluno/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aluno aluno = db.Alunos.Find(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        // POST: Aluno/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NrMatricula,Nome,CPF,DataNascimento,Status")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aluno).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aluno);
+        }
+
+        // GET: Aluno/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aluno aluno = db.Alunos.Find(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        // POST: Aluno/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Aluno aluno = db.Alunos.Find(id);
+            db.Alunos.Remove(aluno);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebSite/Controllers/UsuarioController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DAO.Dao;
+using Model.Models;
+
+namespace WebSite.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private EscolaWebContext db = new EscolaWebContext();
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View(db.Usuarios.ToList());
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Login,Senha,Email")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuario/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Login,Senha,Email")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuarios.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Usuario usuario = db.Usuarios.Find(id);
+            db.Usuarios.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
