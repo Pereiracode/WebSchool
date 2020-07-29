7390a26e894a2bd34002a7c08e04cf1238e7da9d
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-07-29 17:07:21 -0300

    Login com Session

---
+++ WebSite/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using DAO.Utils;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        private HttpClient client = new HttpClient();
+
+        public LoginController()
+        {
+            client.BaseAddress = new Uri("http://localhost:56545/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autenticar(LoginViewModel loginviewmodel)
+        {
+            HttpResponseMessage resposta = client.GetAsync($"api/usuarios/{loginviewmodel.Login}").Result;
+            Usuario usuario = resposta.Content.ReadAsAsync<Usuario>().Result;
+
+            if (usuario != null)
+            {
+                loginviewmodel.Senha = CriptoHash.GerarHash(loginviewmodel.Senha);
+
+                if (loginviewmodel.Senha.Equals(usuario.Senha))
+                {
+                    Session["userLogin"] = usuario.Login;
+                    return RedirectToAction("Index", "Usuario");
+                }
+                else
+                {
+                    ModelState.AddModelError("IdErro", "Usuário e/ou Senha inválidos");
+                    return View("Index");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("IdErro", "Usuário e/ou Senha inválidos");
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ WebSite/Models/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login:")]
+        [MinLength(6, ErrorMessage = "O campo login deve conter no mínimo 6 caracteres")]
+        [MaxLength(12, ErrorMessage = "O campo login deve conter até 12 caracteres")]
+        public string Login { get; set; }
+
+        [Required]
+        [Display(Name = "Senha:")]
+        [MinLength(6, ErrorMessage = "O campo senha deve conter no mínimo 6 caracteres")]
+        [MaxLength(12, ErrorMessage = "O campo senha deve conter até 12 caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
